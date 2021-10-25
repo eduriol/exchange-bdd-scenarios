ac1933c23e7356bcbc7ead78f342077c5fb4c7ee
Eduardo Riol <eriol@ebay.com>
2021-10-25 13:58:12 +0200

    Removing unused commented code

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -6,24 +6,8 @@ mod steps;
 #[tokio::main]
 async fn main() {
     Cucumber::<crate::domain::ExchangeWorld>::new()
-        // Specifies where our feature files exist
         .features(&["./tests/features"])
-        // Adds the implementation of our steps to the runner
         .steps(steps::steps())
-        // Add some global context for all the tests, like databases.
-        //.context(Context::new().add(pool))
-        // Add some lifecycle functions to manage our database nightmare
-        //.before(feature("Example feature"), |ctx| {
-        //    let pool = ctx.get::<SqlitePool>().unwrap().clone();
-        //    async move { create_tables(&pool).await }.boxed()
-        //})
-        //.after(feature("Example feature"), |ctx| {
-        //    let pool = ctx.get::<SqlitePool>().unwrap().clone();
-        //    async move { drop_tables(&pool).await }.boxed()
-        //})
-        // Parses the command line arguments if passed
-        .cli()
-        // Runs the Cucumber tests and then exists
         .run_and_exit()
         .await
 }
